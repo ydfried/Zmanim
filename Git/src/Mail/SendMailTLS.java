26ae80b893712d9a1d0f64e81a806b66eb15f544
ydfried <ydfried@gmail.com>
2016-02-03 18:40:37 -0500

    Testing the email app.

---
+++ Git/src/Mail/SendMailTLS.java
@@ -0,0 +1,56 @@
+package Mail;
+
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMailTLS {
+
+	public static void main(String[] args) {
+		SubFile mailList = new SubFile();
+		StringBuilder sb = new StringBuilder(mailList.match());
+		String subs = sb.toString();
+		final String username = "zmanimproject@gmail.com";
+		final String password = "zmanim2016";
+
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "smtp.gmail.com");
+		props.put("mail.smtp.port", "587");
+
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+		
+		
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress("zmanimproject@gmail.com"));
+			message.setRecipients(Message.RecipientType.BCC,
+				InternetAddress.parse(subs));
+			message.setSubject("Testing Subject");
+			message.setText("Dear Mail Crawler,"
+				+ "\n\n No spam to my email, please!");
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+}
---
+++ Git/src/Mail/SubFile.java
@@ -0,0 +1,44 @@
+package Mail;
+
+import java.io.*;
+
+public class SubFile {
+	
+	
+	public StringBuilder match(){
+
+		StringBuilder sub = new StringBuilder();
+
+		// The name of the file to open.
+        InputStream filePath = this.getClass().getResourceAsStream("subscriber.txt");
+        
+        // This will reference one line at a time
+        String line = null;
+
+        try {
+            
+            // Always wrap FileReader in BufferedReader.
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(filePath, "UTF-8"));
+
+            while((line = bufferedReader.readLine()) != null) {
+                sub.append("," + line);
+            }   
+
+            // Always close files.
+            bufferedReader.close();         
+        }
+        catch(FileNotFoundException ex) {
+            System.out.println(
+                "Unable to open file '" + 
+                filePath + "'");                
+        }
+        catch(IOException ex) {
+            System.out.println(
+                "Error reading file '" 
+                + filePath + "'");                  
+            // Or we could just do this: 
+            // ex.printStackTrace();
+        }
+		return sub;
+    }
+}	
