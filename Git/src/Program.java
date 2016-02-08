89ec703a1f0ee922dc3aa7bc9c343f9fbfa73694
Levi Perelmuter <shortylbc@gmail.com>
2016-02-07 23:38:24 -0500

    SMS functionality

--- Git/src/Program.java
+++ Git/src/Program.java
@@ -16,13 +16,18 @@ public class Program {
 //		cmp.setGeoLocation(testloc);
 //		System.out.println(testloc.toString());
 //		
-//		
-//		
+		try {
+			SMS.test("9175620150", "this is a Test");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 //		System.out.println(cmp.getSofZmanShmaMGA16Point1Degrees()); //dddcfds
 
-		String timeZone = TimeZoneMapper.latLngToTimezoneString(40.6639, -73.9399);
+		//String timeZone = TimeZoneMapper.latLngToTimezoneString(40.6639, -73.9399);
 		
-		UsersReminder LeviPerelmuter = new UsersReminder("Brooklyn, NY", 40.6639, -73.9399,TimeZone.getTimeZone(timeZone),true, true, true, true, true, true);
+		//UsersReminder LeviPerelmuter = new UsersReminder("Brooklyn, NY", 40.6639, -73.9399,TimeZone.getTimeZone(timeZone),true, true, true, true, true, true);
 		
 		
 		
